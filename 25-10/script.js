b257a94c44cd1cd94a42b134346ab5bf5f28bec1
bektursun <koichumanov.bektursun.pi.2.19@gmail.com>
2022-10-25 11:15:57 +0600

    feat:25-10

---
+++ 25-10/script.js
@@ -0,0 +1,16 @@
+let a = 17;
+let b = 10;
+let c = a - b ;
+let d = 7 + c;
+let result = d;
+alert(result);
+
+let hour = prompt('час');
+let minut = prompt('минута');
+let second = prompt('секунда');
+alert(`сейчас ${hour}:${minut}:${second} времени`);
+
+let num = 10;
+    num = ++num;
+    num = ++num;
+alert(num);
