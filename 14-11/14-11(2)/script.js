6b7b08a44060f3a4e892f343eb7c76e4faf7c43b
bektursun <koichumanov.bektursun.pi.2.19@gmail.com>
2022-11-16 09:17:36 +0600

    feat:15-11

--- 14-11/14-11(2)/script.js
+++ 14-11/14-11(2)/script.js
@@ -2,32 +2,46 @@ const btn1 = document.querySelector('#btn1');
 const btn2 = document.querySelector('#btn2');
 const btn3 = document.querySelector('#btn3');
 const notification = document.querySelector('#notification');
+const icon = document.querySelector('#icon');
+const qwerty = document.getElementById('qwerty');
 
 const showNotification = (options) => {
+    notification.classList.remove('d-none');
     notification.classList.add(options.class);
-    setTimeout(deleteNotification, 1500)
+    setTimeout(deleteNotification, 3000);
 }
+const showIcon = (x) => {
+    icon.classList.remove('d-none');
+    icon.classList.add(x.class);
+    setTimeout(deleteNotification, 3000);
+}
+
 btn1.addEventListener('click', () => {
-    showNotification({
-        class:'primary',
-        text: 'A simple primary alert—check it out!'
-    })
+    showNotification({class:'primary'});
+    showIcon({class:'icon'});
+    notification.textContent ='A simple primary alert—check it out!';
 })
 
 btn2.addEventListener('click', () => {
-    showNotification({
-        class:'secondary',
-        text: 'A simple secondary alert—check it out!'
-    })
+    showNotification({class:'secondary'});
+    showIcon({class:'icon'});
+    notification.textContent = 'A simple secondary alert—check it out!';
 })
 
 btn3.addEventListener('click', () => {
-    showNotification({
-        class:'success',
-        text: 'A simple success alert—check it out!'
-    })
+    showNotification({class:'success'});
+    showIcon({class:'icon'});
+    notification.textContent = 'A simple success alert—check it out!';
+})
+
+icon.addEventListener('click', () => {
+    deleteNotification()
 })
 
 const deleteNotification = () => {
     notification.classList.remove('primary', 'secondary', 'success' );
+    icon.classList.add('d-none');
+    notification.textContent = '';
 }
+
+qwerty.style.width = '400px';
